82e14f9031f68005fadffa2f20f446a0d9dbcb77
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-09-06 11:54:11 -0400

    added data

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import Cards from './SoccerCards.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      stats: [],      
+
+
+    }
+  };
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:5000/api/players`)
+      // .then(res => console.log(1, res.data))
+      .then(res => this.setState({ stats: res.data }))
+
+      // .catch(err => console.log('noooo'));
+
+   
+  }
+
+
+  render() {
+    return (
+ 
+  <div className="stats">
+      {this.state.stats.map(stats => (
+        <Cards
+        stats={stats}
+          
+        />
+
+
+      ))}
+        
+    </div >
+    )
+
 }
+}
+
 
 export default App;
---
+++ client/src/SoccerCards.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+class Cards extends React.Component {
+    render(){
+    return(
+        
+        <div statcard>
+            
+            <h1>Name: {this.props.stats.name}</h1>
+            <h1>Country: {this.props.stats.country}</h1>
+            <h1>Searches: {this.props.stats.searches}</h1>
+            
+
+         
+      </div>
+        
+    )
+}
+}
+
+export default Cards;
